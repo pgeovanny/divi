e4090360c325c7f396c0d454b37079608cbf297f
pgeovanny <geovanny9@gmail.com>
2025-07-08 18:41:42 -0300

    Create index.js

---
+++ frontend/pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{display:"flex",flexDirection:"column",alignItems:"center",marginTop:"3rem"}}>
+      <h1 style={{fontSize:"2rem",color:"#0057FF"}}>Gabarite a Lei</h1>
+      <p>Bem-vindo ao simulador legislativo! Faça login para começar.</p>
+    </main>
+  )
+}
